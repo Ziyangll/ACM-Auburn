34c74afb4d5752a6fa00a6c11f21ac861928f2ee
Ziyang Li <ZL@auburn.edu>
2021-04-14 20:48:13 -0500

    NavBar update for mobile

--- acm-auburn/components/Footer.js
+++ acm-auburn/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
 	return (
 		<footer className='text-center'>
 			<div className='socials'>
-				<button className='fixed object-right-bottom bottom-6 right-6 px-2 py-3 rounded-2xl shadow bg-gray-100 hover:bg-white'>
+				<button className='fixed object-right-bottom bottom-6 right-6 px-0 py-0 md:px-2 md:py-3 rounded-2xl shadow bg-gray-100 hover:bg-white'>
 					<a href='https://discord.gg/prNnwtV'>
 						<img src='/discord.svg' alt='discord'></img>
 					</a>
--- acm-auburn/components/NavBar.js
+++ acm-auburn/components/NavBar.js
@@ -3,12 +3,14 @@ import Link from "next/link";
 export default function NavBar() {
 
 	return (
-		<nav className='flex justify-between items-center py-4 px-6'>
+		<nav className='flex justify-between flex-col md:flex-row items-center header py-4 px-6'>
+			<div className="flex" >
 			<Link href='/'>
 				<a className='rounded-xl shadow hover:bg-white py-2 px-2 mx-2 transition ease-in duration-200'>
 					<p className='text-2xl font-bold'><span style={{color: "#03244d"}}>ACM</span> <span style={{color: "#dd550c"}} >AUBURN</span></p>
 				</a>
 			</Link>
+			</div>
 			<div className='flex text-xs md:text-base'>
 				<Link href='/about'>
 					<a className='rounded-xl shadow hover:bg-white py-2 px-1 mx-1 md:px-2 md:mx-2 transition ease-in duration-200'>
